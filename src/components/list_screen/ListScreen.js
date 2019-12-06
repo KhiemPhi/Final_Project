4f2b1bc379a9de0614c7d96daf2540f4e02ef7c0
Khiem_Phi <khiemais@gmail.com>
2019-12-06 14:45:23 -0500

    Adding New Components On the Left Side of Toolbar

--- src/components/list_screen/ListScreen.js
+++ src/components/list_screen/ListScreen.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { compose } from "redux";
 import ItemsList from "./ItemsList.js";
 import { firestoreConnect } from "react-redux-firebase";
-import { Button, Icon } from "react-materialize";
+import { Button, Icon, TextInput } from "react-materialize";
 import "materialize-css/dist/css/materialize.min.css";
 import { getFirestore } from "redux-firestore";
 import { Link } from "react-router-dom"; //
@@ -138,97 +138,104 @@ class ListScreen extends Component {
               </div>
             </div>
 
-            <div>
-              <Button
-                flat
-                className="white container_button"
-                id="container_button"
-                style={{
-                  border: "1px solid black",
-                  height: "70px",
-                  width: "70%",
-                  margin: " 0% 0% 0% 15%"
-                }}
-              ></Button>
-            </div>
+            <Button
+              flat
+              className="white container_button"
+              id="container_button"
+              style={{
+                border: "1px solid black",
+                height: "70px",
+                width: "70%",
+                margin: " 0% 0% 0% 15%"
+              }}
+            ></Button>
+
             <div className="container_label">Container</div>
-            <div>
-              <Button
-                flat
-                className="transparent label_button"
-                id="label_button"
-                style={{
-                  height: "70px",
-                  margin: " 0% 0% 5% 6%"
-                }}
-              >
-                Prompt For Input
-              </Button>
-              
-            </div>
-            <div className="label_label">Label</div>
+
+            <Button
+              flat
+              className="transparent label_component"
+              id="label_button"
+              style={{
+                height: "30px",
+                width: "100%",
+                margin: " 0% 0% 0% 2%"
+              }}
+            >
+              Prompt For Input:
+            </Button>
+            <div className="container_label">Label</div>
+
+            <Button
+              flat
+              className="grey lighten-1 button_component"
+              id="button_creator"
+              style={{
+                height: "30px",
+                width: "80%",
+                margin: " 0% 0% 0% 10%",
+                border: "1px solid black"
+              }}
+            >
+              Submit
+            </Button>
+            <div className="container_label">Button</div>
+
+            <Button
+              flat
+              className="white button_component"
+              id="label_button"
+              style={{
+                height: "30px",
+                width: "100%",
+                margin: " 0% 0% 0% 2%",
+                border: "1px solid black",
+                color: "gray",
+                textAlign: "left"
+              }}
+            >
+              Input
+            </Button>
+            <div className="container_label_padding">Textfield</div>
           </div>
-          <div className=" control_container_only_top col s8">
-            <div className="row">
-              <div className="col s2">
-                <Button
-                  flat
-                  icon={<Icon children="zoom_in" />}
-                  className="transparent"
-                  small
-                ></Button>
-              </div>
-              <div className="col s2">
-                <Button
-                  flat
-                  icon={<Icon children="zoom_in" />}
-                  className="transparent"
-                  small
-                ></Button>
-              </div>
-              <div className="col s2">
-                <Button flat className="transparent" small>
-                  Save
-                </Button>
-              </div>
-              <div className="col s2">
-                <Button flat className="transparent" small>
-                  Close
-                </Button>
-              </div>
-            </div>
-            <div>Second Screen</div>
+          <div className="control_container_only_top white col s8">
+            <div></div>
           </div>
-          <div className=" control_container col s2">
-            <div className="row">
-              <div className="col s2">
-                <Button
-                  flat
-                  icon={<Icon children="zoom_in" />}
-                  className="transparent"
-                  small
-                ></Button>
-              </div>
-              <div className="col s2">
-                <Button
-                  flat
-                  icon={<Icon children="zoom_in" />}
-                  className="transparent"
-                  small
-                ></Button>
-              </div>
-              <div className="col s2">
-                <Button flat className="transparent" small>
-                  Save
-                </Button>
-              </div>
-              <div className="col s2">
-                <Button flat className="transparent" small>
-                  Close
-                </Button>
+          <div className="control_container col s2">
+            <div style ={{paddingTop: "15%", marginLeft: "20%"}} > Properties </div>
+            <div id="item_text">
+              <TextInput id = "text_input" />
+              <div className = "row" style ={{paddingTop: "5%"}}>
+                <div className = "col s8" style={{marginTop: "25px"}}>Font Size</div>
+                <div className = "col s4">
+                  <input id = "font_size_input"></input>
+                </div>
+                <div className = "col s8" style={{marginTop: "25px"}}>Background</div>
+                <div className = "col s1">
+                  <div className = "background_color"></div>
+                </div>
+                <div className = "col s8" style={{marginTop: "25px"}}>Border Color</div>
+                <div className = "col s1">
+                  <div className = "border_color"></div>
+                </div>
+                <div className = "col s8" style={{marginTop: "25px"}}>Text Color</div>
+                <div className = "col s1">
+                  <div className = "text_color"></div>
+                </div>
+                <div className = "col s9" style={{marginTop: "25px", fontSize: "12px"}}>Border Thickness</div>
+                <div className = "col s3 ">
+                  <input id = "font_size_input"></input>
+                </div>
+                <div className = "col s9" style={{marginTop: "25px", fontSize: "12px", paddingBottom: "100%"}}>Border Radius</div>
+                <div className = "col s3 ">
+                  <input id = "font_size_input"></input>
+                </div>
+                
               </div>
             </div>
-            <div>Second Screen</div>
+            <div className row>
+              <div className="col s6"></div>
+            </div>
           </div>
         </div>
       </div>
